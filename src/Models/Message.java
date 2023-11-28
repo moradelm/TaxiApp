89f836a8d42251c44f94f4ad24fb5fd5469aecaf
amina-bl <aminaboulamaat4@gmail.com>
2023-11-28 19:38:16 +0100

    initial commit + exemples de classes

---
+++ src/Models/Message.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class Message {
+	
+	private int id;
+	private Reservation reservation;
+	private String message;
+	
+}
---
+++ src/Models/Reservation.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class Reservation {
+	
+	private int id;
+	
+	//TODO:Ajouter les autres attributs aprés
+	
+}
