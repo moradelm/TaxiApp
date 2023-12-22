06492c76ac366c6180507c04e70e5654465e7e64
amina-bl <aminaboulamaat4@gmail.com>
2023-12-22 21:07:26 +0100

    add model databaseConnexion

---
+++ Taxi/src/main/java/com/example/taxi/BdConnexion.java
@@ -0,0 +1,48 @@
+package com.example.taxi;
+import java.sql.*;
+
+public class BdConnexion {
+
+		private String driver="com.mysql.jdbc.Driver";
+		private String url="jdbc:mysql://localhost:3306/taxiapp";
+		private String user="root";
+		private String pwd="";
+		private Connection con;
+		private static Statement st;
+		public BdConnexion()  {
+			try {
+				Class.forName(driver);
+			}catch(ClassNotFoundException ex){
+				System.out.println("Probleme driver");
+			}
+			try {
+				con=DriverManager.getConnection(url,user,pwd);
+	            st=con.createStatement();
+			}catch(SQLException ex) {
+				System.out.println("Probleme conn");
+			}
+		}
+		
+		public  int executerAction(String s)  {
+			int nbr=0;
+			try {
+				nbr=st.executeUpdate(s);
+				
+			}catch(SQLException e) {
+				System.out.println("Probleme req");
+
+			}
+			return nbr;
+		}
+		
+		public ResultSet executerSelect(String s) {
+			ResultSet rs=null;
+			try {
+				rs=st.executeQuery(s);
+			}catch(SQLException e) {
+				System.out.println("probleme req select");
+			}
+			return rs;
+		}
+
+}
--- Taxi/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.taxi {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.taxi to javafx.fxml;
-    exports com.example.taxi;
-}
